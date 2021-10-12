a8b76b52e9fc52e3f5b47cfe67a25862d9b79780
ChumaDenis <chumiks1999@gmail.com>
2021-10-12 23:54:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    enum Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+    public class User
+    {
+        private string ip;
+        private DateTime time;
+        private string day;
+        private int count = 1;
+        public int Count 
+        {
+            set { count=value; }
+            get { return count; }
+        }
+        public string Ip 
+        { 
+            set { this.ip = value; }
+            get { return ip; } 
+        }
+        public DateTime Time
+        {
+            set { this.time = value; }
+            get { return time; }
+        }
+        public string Day
+        {
+            set { this.day = value; }
+            get { return day; }
+        }
+        public User(string str)
+        {
+            string[] tmp = str.Split(' ');
+            ip= tmp[0];
+            time = Convert.ToDateTime(tmp[1]);
+            day = tmp[2];
+        }
+    }
+
+    class Check
+    {
+        private User[] A;
+        public Check(string path)
+        {
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    string[] def = sr.ReadToEnd().Split('\n');
+                    A = new User[def.Length];
+                    for (int i = 0; i < def.Length; i++)
+                    {
+                        for (int k = 0; k < i; k++)
+                        {
+                            if (def[i].Split(' ')[0] == A[k].Ip)
+                            {
+                                A[k].Count++;
+                                A[i] = new User(def[i]);
+                                A[i].Count = A[k].Count++;
+                            }
+                            else
+                                A[i] = new User(def[i]);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong {ex}");
+            }
+        }
+
+        public Day MostPopularDay()
+        {
+            int MaxPop = 0;
+            Day MaxDay= Day.Monday;
+            Day tempDay = Day.Monday;
+            for (int i = 0; i<7;i++ , tempDay++)
+            {
+                int count = 0;
+                for (int n = 0; n < A.Length; n++)
+                {
+                    if(A[n].Day==tempDay.ToString())
+                    {
+                        count++;
+                    }
+                }
+                if (MaxPop <count)
+                {
+                    MaxPop = count;
+                    MaxDay = tempDay;
+                }
+            }
+            return MaxDay;
+        }
+
+        public string MaxHour(Day tempDay)
+        {
+            int MaxCount = 0;
+            DateTime MaxTime = new DateTime(0, 0, 0);
+            DateTime TempTime = new DateTime(0, 0, 0);
+            for (int i = 0; i < 24; i++, TempTime += TimeSpan.FromHours(1))
+            {
+                int count = 0;
+                for (int n = 0; n < A.Length; n++)
+                {
+                    if((tempDay.ToString()==A[n].Day)&&(TempTime>=A[n].Time && A[n].Time<=(TempTime+ TimeSpan.FromHours(1))))
+                    {
+                        count++;
+                    }
+                }
+                if(MaxCount<count)
+                {
+                    MaxCount = count;
+                    MaxTime = TempTime;
+                }
+            }
+            return MaxTime.ToString();
+        }
+    }
+    
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path(to log file):");
+            string path = @"";
+            path+=Console.ReadLine();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Polynomial
+    {
+        public double x { set; get; }
+        public double[] c;
+        
+
+        public Polynomial()
+        {
+
+        }
+        public Polynomial(int n)
+        {
+            c = new double[n];
+        }
+
+        public void Parse(string s)
+        {
+            string[] buf = s.Split(' ', '*');
+            c = new double[buf.Length -2];
+            //неявне приведення до double
+            c[0] = float.Parse(buf[0]);
+            for (int i=1, n=1; i<buf.Length; n++)
+            {
+                c[n] = Convert.ToDouble(buf[i]);
+                i += 2;
+                Console.WriteLine(c[n]);
+            }
+        }
+
+
+        public void Output()
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (i == 0)
+                    Console.Write(String.Format("{0} ", c[i]));
+                else
+                {
+                    if (c[i]>0)
+                        Console.Write(String.Format("+{0}*x^{1} ", c[i], i));
+                    else
+                        Console.Write(String.Format("{0}*x^{1} ", c[i], i));
+                }
+            }
+        }
+        public void Checked()
+        {
+            double Sum = 0;
+            for(int i=0; i<c.Length; i++)
+            {
+                if (c[i]!=0 && c[i]*Math.Pow(x, i)!=0)
+                {
+                    c[i] = c[c.Length-2]* Math.Pow(x, c.Length-3);
+                }
+                else if(c[i] != 0 && c[i] * Math.Pow(x, i) == 0)
+                {
+                    
+                    Array.Resize(ref c, c.Length+1);
+                    c[c.Length] = c[c.Length - 2] * Math.Pow(x, c.Length - 3);
+                }
+                else if(c[i] == 0 && c[i] * Math.Pow(x, i) != 0)
+                {
+                   
+                }
+                else if(c[i] == 0 && c[i] * Math.Pow(x, i) == 0)
+                {
+
+                }
+            }
+        }
+
+        public static Polynomial operator +(Polynomial a)
+        {
+            Polynomial Temp = new Polynomial(c.Length>a.c.Length? c.Length: a.c.Length);
+
+            for(int i=0; i< (c.Length < a.c.Length ? c.Length : a.c.Length); i++)
+            {
+               Temp.c[i]=c[i]+a.c[i];
+            }
+            double[] r = c.Length > a.c.Length ? c : a.c;
+            for(int i= (c.Length < a.c.Length ? c.Length : a.c.Length)-1; i<r.Length; i++)
+            {
+                Temp.c[i] = r[i];
+            }
+            return Temp;
+        }
+        public static Polynomial operator -(Polynomial a)
+        {
+            Polynomial Temp = new Polynomial(c.Length >= a.c.Length ? c.Length : a.c.Length);
+
+            for (int i = 0; i < (c.Length < a.c.Length ? c.Length : a.c.Length); i++)
+            {
+                Temp.c[i] = c[i] - a.c[i];
+            }
+            double[] r = c.Length > a.c.Length ? c : a.c;
+            for (int i = (c.Length < a.c.Length ? c.Length : a.c.Length) - 1; i < r.Length; i++)
+            {
+                if (c.Length > a.c.Length)
+                    Temp.c[i] = r[i];
+                else if (c.Length < a.c.Length)
+                    Temp.c[i] = -r[i];
+            }
+            return Temp;
+        }
+        public static Polynomial operator *(Polynomial a)
+        {
+            Polynomial Temp = new Polynomial(c.Length+a.c.Length);
+            for (int i = 0; i <= c.Length; i++)
+            {
+                for (int j = 0; j <= a.c.Length; j++)
+                {
+                    Temp.c[i + j] += c[i] * a.c[j];
+                }
+            }
+
+            return Temp;
+        }
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "-12 +3*x^1 -5*x^2";
+            Polynomial b = new Polynomial();
+            b.Parse(a);
+            b.Output();
+        }
+    }
+}
